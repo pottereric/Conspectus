b2ab38374b08aace527af9de06287f38b15c5bad
Eric Potter <eric.b.potter@gmail.com>
2012-06-26 13:50:39 -0400

    initial commit

---
+++ Conspectus.Model/ClassConspectus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conspectus.Model
+{
+    public class ClassConspectus
+    {
+        public IEnumerable<MethodConspectus> Methods { get; set;}
+        public IEnumerable<PropertyConspectus> Properties{ get; set;}
+        public IEnumerable<FieldConspectus> Fields { get; set;}
+        public IEnumerable<EventConspectus> Events{ get; set;}
+    }
+
+    public class MemberConspectus
+    {
+        public string Identifier { get; set; }
+    }
+
+    public class MethodConspectus : MemberConspectus
+    {
+    }
+
+    public class PropertyConspectus : MemberConspectus
+    {
+
+    }
+
+    public class FieldConspectus : MemberConspectus
+    {
+
+    }
+
+    public class EventConspectus : MemberConspectus
+    {
+
+    }
+}
---
+++ Conspectus/SourceAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roslyn.Compilers.CSharp;
+
+namespace Conspectus
+{
+    class SourceAnalyzer
+    {
+        protected string code;
+
+        public SourceAnalyzer(string code)
+        {
+            this.code = code;
+        }
+
+        protected SyntaxList<MemberDeclarationSyntax> GetClassNodes()
+        {
+            // Have Roslyn Parse the file
+            var tree = SyntaxTree.ParseCompilationUnit(this.code);
+            CompilationUnitSyntax compilationUnit = (CompilationUnitSyntax)tree.Root;
+
+            // TODO: don't hard code [0]
+            // Get the Namespace node
+            NamespaceDeclarationSyntax namespaceDeclaration = (NamespaceDeclarationSyntax)compilationUnit.Members[0];
+
+            // Get the class node
+            return namespaceDeclaration.Members;
+        }
+    }
+}
